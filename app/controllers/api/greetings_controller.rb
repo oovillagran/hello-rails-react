5fd92f7bd23392d77f532d6527f692558ddb22bf
oovillagran <oscar.villagran@hotmail.com>
2023-08-22 02:22:04 -0500

    Add routes

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,2 +1,6 @@
 class Api::GreetingsController < ApplicationController
+  def greetings
+    @greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: @greeting.message }
+  end
 end
--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,4 @@
 class GreetingsController < ApplicationController
+  def def index
+  end
 end
